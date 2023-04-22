979f074f293f551e968b6fe784c6a2bbeef2e3aa
Kholoudxs55kh <kfattem@gmail.com>
2023-04-22 11:33:44 -0700

    trying

--- _printf.c
+++ _printf.c
@@ -8,11 +8,22 @@
 
 int _printf(const char *format, ...)
 {
+	int x;
+
 	va_list args;
 
 	va_start(args, format);
 
-	vprintf(format, args);
-
-	va_end(args);
+		x = 0;
+		while ((format != NULL) && (format[x] != '\0') && (format[x] == '%'))
+		{
+			switch (format[x + 1])
+			{
+			case 'c':
+				write(1, format, 1);
+					break;
+			}
+			x++;
+		}
+		return (0);
 }
