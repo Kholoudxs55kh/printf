c94cd886a7ce6a9a1fd715cc11131d374be26428
Kholoudxs55kh <kfattem@gmail.com>
2023-04-22 10:53:30 -0700

    Trying

--- _printf.c
+++ _printf.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <stdarg.h>
-
+/**
+ * _printf - amplimition to printf
+ * @format: string
+ * Return: whatever
+ */
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start (args, format);
-    vprintf(format, args);
-    va_end(args);
+	va_list args;
+
+	va_start(args, format);
+
+	vprintf(format, args);
+
+	va_end(args);
 }
