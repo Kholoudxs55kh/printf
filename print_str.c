c2dea0dd48e8d9a290952179e98ccdee292da1d8
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-24 03:40:07 +0100

    anything

--- print_str.c
+++ print_str.c
@@ -41,10 +41,9 @@ char *rot13(char *k)
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	int h = 0, m = 0;
-	char t[] = "NULL";
 
 	if (!k)
-		k = t;
+		return (0);
 	while (k[h] != '\0')
 	{
 		for (m = 0; m < 54; m++)
