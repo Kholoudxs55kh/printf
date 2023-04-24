fe7c5365210d4ce0746c84d3fbbfb160272baf25
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-24 04:45:20 +0100

    anything

--- _printf.c
+++ _printf.c
@@ -22,8 +22,6 @@ int helpswitch(char s, va_list args, int *len)
 			break;
 		case 'r':
 			print_rev(va_arg(args, char *), len);
-			if (!args)
-				*len += write(1, "%r", 2);
 			break;
 		default:
 			return (1);
