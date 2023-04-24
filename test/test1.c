41af547e08c2f3909718085e309f7f8bececec03
Kholoudxs55kh <kfattem@gmail.com>
2023-04-23 22:22:03 -0700

    AddingMainTestFile

--- test/test1.c
+++ test/test1.c
@@ -15,12 +15,10 @@ int main(void)
 	len2 = printf("");
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
-
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
-
 	len = _printf("%s\n", "emad");
 	len2 = printf("%s\n", "emad");
 	printf("len %d\n", len);
@@ -29,23 +27,17 @@ int main(void)
 	len2 = printf("%%\n");
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
-
-
 	len = _printf(NULL);
 	len2 = printf(NULL);
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
-
 	len = _printf(NULL, NULL);
 	len2 = printf(NULL, NULL);
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
-
-
 	len = _printf("%s %c %%\n", NULL);
 	len2 = printf("%s %c %%\n", NULL);
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
 	return 0;
 }
-
